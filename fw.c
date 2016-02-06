f6e1a0ca9cd892adc491307e3b201eae52216d3a
あるマイト <universal5b@hotmail.co.jp>
2016-02-06 14:44:37 +0900

    change header_name
    in6.hだった可能性

--- fw.c
+++ fw.c
@@ -9,7 +9,7 @@
 #include <netpacket/packet.h> //struct sockaddr_ll
 #include <poll.h>
 
-#include <netinet6/ip6.h>
+#include <netinet6/in6.h>
 
 char *ip_ntoa(u_int32_t ip) {
 	u_char *d = (u_char *)&ip;
